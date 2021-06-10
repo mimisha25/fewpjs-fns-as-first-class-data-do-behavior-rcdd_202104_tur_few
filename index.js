50ca6d943948f790b06138a871ac7c8c7cbacb56
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-10 23:55:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
-
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+
 /* Write your implementation of displayMessage() */
+displayMessage("index.js", () => {
+displayMessage("greet() function", () => {
+    it("Returns Good Morning before 12pm", () => {
+      expect(greet("8:40")).to.equal('Good Morning')
+    })
+    it("Returns Good Afternoon between 12pm and 5pm ", () => {
+      expect(greet("14:21")).to.equal('Good Afternoon')
+    })
+    it("Returns Good Evening after 5pm", () => {
+      expect(greet("19:00")).to.equal('Good Evening')
+    })
+  })
