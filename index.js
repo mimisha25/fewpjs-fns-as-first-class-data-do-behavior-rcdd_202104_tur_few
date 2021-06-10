904ebe6c708c206f3403cb5fe38fd2fc36328797
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-10 23:58:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-/* Given Code, don't edit */
+function greet(time) {
+   let greeting;
+   let st = time.split(':');
+   let hour = Number(st[0]);
 
-document.querySelector("button").addEventListener("click", handleClick);
+   if (hour < 12){
+       greeting = "Good Morning";
+   }
+     else if (hour <= 17) {
+       greeting =  "Good Afternoon";
+     }
+     else {
+       greeting =  "Good Evening";
+   };
+   return greeting;
+ }
 
-function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
-}
+ /* Write your implementation of displayMessage() */
 
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+ function displayMessage(string){
+   document.getElementById("greeting").innerText = string;
+ }
